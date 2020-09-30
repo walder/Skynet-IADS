9b28e782d88047a9dee4dd29c4c64ca6422420c7
walder <walder@allink.ch>
2020-09-30 20:13:09 +0200

    donate update

--- demo-missions/skynet-iads-compiled.lua
+++ demo-missions/skynet-iads-compiled.lua
@@ -1,4 +1,4 @@
-env.info("--- SKYNET VERSION: 1.1.3 | BUILD TIME: 30.09.2020 1735Z ---")
+env.info("--- SKYNET VERSION: 1.1.3 | BUILD TIME: 30.09.2020 1813Z ---")
 do
 --this file contains the required units per sam type
 samTypesDB = {
