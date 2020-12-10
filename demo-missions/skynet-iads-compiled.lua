695f3c0b9db62b8c0c34ce074f4cf377f46362ec
Mike Walder <walder@allink.ch>
2020-12-10 19:58:27 +0100

    update highdigitsam integration

--- demo-missions/skynet-iads-compiled.lua
+++ demo-missions/skynet-iads-compiled.lua
@@ -1,4 +1,4 @@
-env.info("--- SKYNET VERSION: -2.0.0-develop | BUILD TIME: 03.12.2020 1801Z ---")
+env.info("--- SKYNET VERSION: -2.0.0-develop | BUILD TIME: 10.12.2020 1857Z ---")
 do
 --this file contains the required units per sam type
 samTypesDB = {
@@ -418,6 +418,52 @@ samTypesDB = {
 		['harm_detection_chance'] = 40
 	},	
 }
+end
+do
+-- this file contains the definitions for the HightDigitSAMSs: https://github.com/Auranis/HighDigitSAMs
+
+--[[ units in SA-10 group:
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 54K6 cp
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 5P85CE ln
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 5P85DE ln
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 40B6MD sr
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 64N6E sr
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 40B6M tr
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 30N6E tr
+--]]
+s3000pmu1 = {
+	['type'] = 'complex',
+	['searchRadar'] = {
+		['S-300PMU1 40B6MD sr'] = {
+		},
+		['S-300PMU1 64N6E sr'] = {
+		},
+	},
+	['trackingRadar'] = {
+		['S-300PMU1 40B6M tr'] = {
+		},
+		['S-300PMU1 30N6E tr'] = {
+		},
+	},
+	['misc'] = {
+		['S-300PMU1 54K6 cp'] = {
+			['required'] = true,
+		},
+	},
+	['launchers'] = {
+		['S-300PMU1 5P85CE ln'] = {
+		},
+		['S-300PMU1 5P85DE ln'] = {
+		},
+	},
+	['name']  = {
+		['NATO'] = 'SA-20A Gargoyle'
+	},
+	['harm_detection_chance'] = 90
+}	
+samTypesDB['S-300PMU1'] = s3000pmu1
+
+
 end
 do
 
--- skynet-iads-source/highdigitsams/skynet-iads-high-digit-sams-suported-types.lua
+++ skynet-iads-source/highdigitsams/skynet-iads-high-digit-sams-suported-types.lua
@@ -1,6 +1,15 @@
 do
 -- this file contains the definitions for the HightDigitSAMSs: https://github.com/Auranis/HighDigitSAMs
 
+--[[ units in SA-10 group:
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 54K6 cp
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 5P85CE ln
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 5P85DE ln
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 40B6MD sr
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 64N6E sr
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 40B6M tr
+2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 30N6E tr
+--]]
 s3000pmu1 = {
 	['type'] = 'complex',
 	['searchRadar'] = {
@@ -14,7 +23,7 @@ s3000pmu1 = {
 		},
 		['S-300PMU1 30N6E tr'] = {
 		},
-	}
+	},
 	['misc'] = {
 		['S-300PMU1 54K6 cp'] = {
 			['required'] = true,
@@ -28,18 +37,10 @@ s3000pmu1 = {
 	},
 	['name']  = {
 		['NATO'] = 'SA-20A Gargoyle'
-	}
+	},
 	['harm_detection_chance'] = 90
 }	
 samTypesDB['S-300PMU1'] = s3000pmu1
---[[ units in SAM group:
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 54K6 cp
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 5P85CE ln
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 5P85DE ln
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 40B6MD sr
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 64N6E sr
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 40B6M tr
-2020-12-10 18:27:27.050 INFO    SCRIPTING: S-300PMU1 30N6E tr
---]]
+
 
 end
